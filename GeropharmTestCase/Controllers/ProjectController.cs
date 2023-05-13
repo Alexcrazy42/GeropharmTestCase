a0b9176a705535aff7a12639790ba39d14035174
Alex42 <alexcrazy42@mail.ru>
2023-05-13 23:51:51 +0300

    Добавьте файлы проекта.

---
+++ GeropharmTestCase/Controllers/ProjectController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GeropharmTestCase.Models;
+using GeropharmTestCase.Data;
+
+namespace GeropharmTestCase.Controllers
+{
+    [ApiController]
+    [Route("projects")]
+    public class ProjectController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public ProjectController()
+        {
+            _context = new ApplicationContext();
+        }
+
+
+        [HttpGet("all")]
+        public async Task<ActionResult<IEnumerable<object>>> GetAllProjects() // 
+        {
+            if (_context.Projects == null)
+            {
+                return NotFound();
+            }
+            var projects = from p in _context.Projects
+                           select new
+                           {
+                               Id = p.Id,
+                               Name = p.Name,
+                               Description = p.Description
+                           };
+            return await projects.ToListAsync();
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<object>> GetSomeProjects(int firstId, int count)
+        {
+            if (_context.Projects == null)
+            {
+                return NotFound();
+            }
+
+            var projects = from p in _context.Projects
+                           where p.Id >= firstId && p.Id < (firstId + count)
+                           select new
+                           {
+                               Id = p.Id, 
+                               Name = p.Name,
+                               Descriprion = p.Description
+                           };
+
+            if (projects.ToArray().Length == 0)
+            {
+                return NotFound();
+            }
+
+            return await projects.ToListAsync();
+        }
+
+    }
+}
---
+++ GeropharmTestCase/Data/ApplicationContext.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GeropharmTestCase.Models;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace GeropharmTestCase.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+
+
+
+        public ApplicationContext()
+        {
+        }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connection = "Host=localhost; Port = 5432; Database=VK; Username=postgres; Password=26031974yula;";
+                optionsBuilder.UseNpgsql(connection);
+                optionsBuilder.EnableSensitiveDataLogging();
+            }
+            else
+            {
+
+                var db = new ApplicationContext();
+                if (db.Projects.Count() == 0)
+                {
+                    var firstProject = new Project()
+                    {
+                        Name = "Project" + 1.ToString(),
+                        Description = "Description" + 1.ToString()
+                    };
+                    db.Add(firstProject);
+                    db.SaveChanges();
+                }
+                else if (db.Projects.Count() < 100)
+                {
+                    for (int i = 0; i < 100 - db.Projects.Count(); i++)
+                    {
+                        var lastProject = db.Projects
+                            .OrderByDescending(p => p.Id)
+                            .FirstOrDefault();
+
+                        var newProject = new Project()
+                        {
+
+                            Name = "Project" + (lastProject.Id + 1).ToString(),
+                            Description = "Description" + (lastProject.Id + 1).ToString()
+                        };
+                        db.Add(newProject);
+                        db.SaveChanges();
+
+                    }
+                }
+                else
+                {
+                    var projectToDelete = db.Projects.ToList();
+                    db.Projects.RemoveRange(projectToDelete);
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
